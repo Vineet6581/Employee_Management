23bd38f8fe6995fe497d7df11e4f9cda0bd1e13f
Vineet Kumar <mysteriousid2573@gmail.com>
2026-04-29 02:21:03 +0530

    Fixed production CORS

--- backend/server.js
+++ backend/server.js
@@ -37,3 +37,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+app.use(cors({
+  origin: "https://employee-management-crrh.vercel.app",
+  credentials: true
+}));
